44dd66422904c5ee86d44b511fd84898487214de
Charlye Tran <ctran@wsip-70-165-249-119.lv.lv.cox.net>
2015-01-03 20:48:45 -0800

    show lat and longitude

--- public/js/app.js
+++ public/js/app.js
@@ -36,10 +36,11 @@ var app = {
             var array = app.json.ListPage.pages.BasicPage;
             for(var i=0; i < 10; i++){
                 var spot = $('<div class="spot_' + i + '">').text("Latitude: " + array[i].glat + "Longitude: " + array[i].glong);
-                $("#results").append(spot);
+                ($("#results")).append(spot);
             }
-            $("#results").show();
-        }
+            var results = $("#map").append($("#results"))
+            results.show();
+        }   
     },
     onLocationFound: function (e) {
         var radius = e.accuracy / 2;
